9f13c427122d24aac50e9f65d008b38bfcbb1f41
Duc Nguyen <minhduc140583@gmail.com>
2021-10-30 22:48:59 +0700

    Delete user_service file

--- internal/service/sql_user_service.go
+++ internal/service/sql_user_service.go
@@ -11,6 +11,16 @@ import (
 	. "go-service/internal/model"
 )
 
+
+type UserService interface {
+	All(ctx context.Context) (*[]User, error)
+	Load(ctx context.Context, id string) (*User, error)
+	Insert(ctx context.Context, user *User) (int64, error)
+	Update(ctx context.Context, user *User) (int64, error)
+	Patch(ctx context.Context, user map[string]interface{}) (int64, error)
+	Delete(ctx context.Context, id string) (int64, error)
+}
+
 type SqlUserService struct {
 	DB *sql.DB
 }
